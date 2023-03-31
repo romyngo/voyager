9695609bba6d7f96e27ea15eb65963dbb9e11ab1
romyngo <ngominhquan88@gmail.com>
2023-03-31 19:22:50 +0700

    Build with full plugins

--- resources/assets/js/voyager_tinymce.js
+++ resources/assets/js/voyager_tinymce.js
@@ -7,6 +7,31 @@ import 'tinymce/plugins/image';
 import 'tinymce/plugins/code';
 import 'tinymce/plugins/table';
 import 'tinymce/plugins/lists';
+import 'tinymce/plugins/preview'; 
+import 'tinymce/plugins/importcss'; 
+import 'tinymce/plugins/searchreplace'; 
+import 'tinymce/plugins/autolink'; 
+import 'tinymce/plugins/autosave'; 
+import 'tinymce/plugins/save'; 
+import 'tinymce/plugins/directionality'; 
+import 'tinymce/plugins/visualblocks'; 
+import 'tinymce/plugins/visualchars'; 
+import 'tinymce/plugins/fullscreen'; 
+import 'tinymce/plugins/media'; 
+import 'tinymce/plugins/template'; 
+import 'tinymce/plugins/codesample'; 
+import 'tinymce/plugins/charmap'; 
+import 'tinymce/plugins/pagebreak'; 
+import 'tinymce/plugins/nonbreaking'; 
+import 'tinymce/plugins/anchor'; 
+import 'tinymce/plugins/insertdatetime'; 
+import 'tinymce/plugins/advlist'; 
+import 'tinymce/plugins/wordcount'; 
+import 'tinymce/plugins/help'; 
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/quickbars'; 
+import 'tinymce/plugins/emoticons'; 
+
 
 $(document).ready(function(){
 
